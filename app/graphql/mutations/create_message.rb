df2ac13a569b9c335c60fc567d92a826b32221e0
Arnaud Bouchard <ar.bouchard@gmail.com>
2020-07-23 17:32:19 +0200

    make visibility/text not required

--- app/graphql/mutations/create_message.rb
+++ app/graphql/mutations/create_message.rb
@@ -1,13 +1,13 @@
 class Mutations::CreateMessage < Mutations::BaseMutation
     argument :author, String, required: true
     argument :recipient, String, required: true
-    argument :visibility, String, required: true
-    argument :text, String, required: true
+    argument :visibility, String, required: false
+    argument :text, String, required: false
 
     field :message, Types::MessageType, null: false
     field :errors, [String], null: false
 
-    def resolve(author:, recipient:, visibility:, text:)
+    def resolve(author:, recipient:, visibility: nil, text: nil)
         message = Message.new(author: author, recipient: recipient, visibility: visibility, text: text)
         if message.save
             {
